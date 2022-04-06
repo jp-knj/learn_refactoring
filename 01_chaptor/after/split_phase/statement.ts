5ed3dba29d7ee8c649835fb84e26585af95ad837
jp-knj <kenji.tomita1996@gmail.com>
2022-04-06 21:59:53 +0900

    :tada: split phase

--- 01_chaptor/after/split_phase/statement.ts
+++ 01_chaptor/after/split_phase/statement.ts
@@ -1,21 +1,26 @@
 import plays from '../plays.json';
 import invoice from '../invoices.json';
 
-console.log(statement(invoice[0], plays));
-
 function statement (invoice:any, plays:any) {
     const statementData = {
         customer: '',
-        performance: ''
+        performance: '',
+        totalAmount: '',
+        totalVolumeCredits: '',
     };
+
     statementData.customer = invoice.customer;
     statementData.performance = invoice.performance.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalValueCredits(statementData);
+
     return renderPlainText( statementData, plays);
 
     function enrichPerformance(aPerformance:any) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -44,17 +49,6 @@ function statement (invoice:any, plays:any) {
     function playFor(aPerformance: any) {
         return plays[aPerformance.playID];
     }
-}
-
-function renderPlainText(data:any, plays: any) {
-    let result = `Statement for ${ data.customer }\n`;
-    for (let perf of data.performances) {
-        result += ` ${ perf.play.name }: ${ usd(perf.audience)} (${ perf.audience } seats)\n`;
-    }
-
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalValueCredits()} credits\n`;
-    return result;
 
     function volumeCreditsFor(aPerformance: any){
         let result = 0;
@@ -63,27 +57,30 @@ function renderPlainText(data:any, plays: any) {
         return result;
     }
 
+    function totalAmount(data:any){
+        return data.performance.reduce((total: any, p:any) => total + p.amount, 0)
+    }
+
+    function totalValueCredits(data:any) {
+        return data.performance.reduce((total:any, p:any) => total + p.volumeCredits, 0);
+    }
+}
+
+function renderPlainText(data:any, plays: any) {
+    let result = `Statement for ${ data.customer }\n`;
+    for (let perf of data.performances) {
+        result += ` ${ perf.play.name }: ${ usd(perf.audience)} (${ perf.audience } seats)\n`;
+    }
+
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalValueCredits} credits\n`;
+    return result;
+
     function usd(aNumber:any) {
         return new Intl.NumberFormat("en-US",
             {
                 style: "currency", currency: "USD",
                 minimumFractionDigits: 2
             }).format(aNumber/100);
-    }
-
-    function totalValueCredits() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result
-    }
-
-    function totalAmount(){
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result
     }
 }
