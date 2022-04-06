453c7d7d47431f599fb905365150ddde01a676f0
jp-knj <kenji.tomita1996@gmail.com>
2022-04-07 00:04:33 +0900

    :sparkles: call new func

--- 01_chaptor/after/reorganize_logic/createStatementData.ts
+++ 01_chaptor/after/reorganize_logic/createStatementData.ts
@@ -29,6 +29,13 @@ class PerformanceCalculator {
 
         return result;
     }
+
+    get volumeCredits(){
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+        return result;
+    }
 }
 
 export default function createStatementData(invoice:any, plays:any) {
@@ -49,8 +56,8 @@ export default function createStatementData(invoice:any, plays:any) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
