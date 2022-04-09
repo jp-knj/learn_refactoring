8764fc5c9b08e0fd05e171615c3cf1e799a7eaa5
jp-knj <kenji.tomita1996@gmail.com>
2022-04-09 15:57:07 +0900

    :tada: changes fixture

--- 04_chaptor/before/province.test.js
+++ 04_chaptor/before/province.test.js
@@ -6,13 +6,17 @@ describe('province', () => {
     beforeEach(() => {
         asia = new Province(sampleProvinceData());
     })
-
-  it('shortfail', () => {
+    it('shortfail', () => {
       const asia = new Province(sampleProvinceData());
       expect(asia.shortfall).toEqual(5);
-  })
+    })
     it('profit', () => {
         const asia = new Province(sampleProvinceData());
         expect(asia.profit).toEqual(230);
+    })
+    it('change production', () => {
+        asia.producers[0].production = 20;
+        expect(asia.shortfall).toEqual(-6);
+        expect(asia.profit).toEqual(292);
     })
 })
