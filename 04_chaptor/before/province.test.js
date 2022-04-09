98b6dbdb1dd31e4ed67080dc2d6c086950381f6d
jp-knj <kenji.tomita1996@gmail.com>
2022-04-09 15:49:14 +0900

    :tada: add test  ✨  :sparkles: 新機能（New Feature）

--- 04_chaptor/before/province.test.js
+++ 04_chaptor/before/province.test.js
@@ -4,6 +4,10 @@ import assert from "assert";
 describe('province', () => {
   it('shortfail', () => {
       const asia = new Province(sampleProvinceData());
-      assert.equal(asia.shortfall, 5)
+      expect(asia.shortfall).toEqual(5);
   })
+    it('profit', () => {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.profit).toEqual(230);
+    })
 })
