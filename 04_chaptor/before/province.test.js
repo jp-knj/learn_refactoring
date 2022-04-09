7b079d29bf0909dbdba60966d91123362c92e52f
jp-knj <kenji.tomita1996@gmail.com>
2022-04-09 15:51:07 +0900

    :+1: setup

--- 04_chaptor/before/province.test.js
+++ 04_chaptor/before/province.test.js
@@ -2,6 +2,11 @@ import { Province, sampleProvinceData } from './province'
 import assert from "assert";
 
 describe('province', () => {
+    let asia;
+    beforeEach(() => {
+        asia = new Province(sampleProvinceData());
+    })
+
   it('shortfail', () => {
       const asia = new Province(sampleProvinceData());
       expect(asia.shortfall).toEqual(5);
