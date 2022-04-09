621e017386cc890fcaeb4920d0ebb73f76eef337
jp-knj <kenji.tomita1996@gmail.com>
2022-04-09 16:03:23 +0900

    :sparkles: update them

--- 04_chaptor/before/province.test.js
+++ 04_chaptor/before/province.test.js
@@ -20,3 +20,24 @@ describe('province', () => {
         expect(asia.profit).toEqual(292);
     })
 })
+
+describe('no producers', () => {
+    let noProducers;
+    beforeEach( () => {
+        const data = {
+            name: "No producers",
+            producers: [],
+            demand: 30,
+            price: 20
+        };
+
+        noProducers = new Province(data);
+
+    })
+    it('shortfail', () => {
+        expect(noProducers.shortfall).toEqual(30);
+    })
+    it('profit', () => {
+        expect(noProducers.profit).toEqual(0);
+    })
+})
